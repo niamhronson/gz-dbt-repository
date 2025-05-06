189e8627610d3b9656377bdead34052b9a5f4978
niamhronson@gmail.com <niamhronson@gmail.com>
2025-05-06 15:26:07 +0000

    Operational margin update

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,5 @@
 SELECT
-orders_id,
-date_date,
+*,
 round(margin + s.shipping_fee - s.logcost - cast(s.ship_cost as INT64),2) AS operational_margin
 FROM {{ ref("int_orders_margin")}} as o
 LEFT JOIN {{ ref("stg_raw__ship")}} as s
